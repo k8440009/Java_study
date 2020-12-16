3d8685e29e3765bdc7ae465a0b8f03884c8c77f9
sungslee <k8440009@gmail.com>
2020-12-16 17:23:14 +0900

    add Exercise7_5

---
+++ JavaStandard/Chapter7/Exercise/Exercise7_5/Exercise.java
@@ -0,0 +1,28 @@
+package JavaStandard.Chapter7.Exercise.Exercise7_5;
+
+class Product {
+	int price;
+	int bonusPoint;
+
+	Product(){}
+	
+	Product(int price){
+		this.price = price;
+		bonusPoint = (int)(price / 10.0);
+	}
+}
+
+class Tv extends Product{
+
+	Tv(){}
+
+	public String toString(){
+		return "Tv";
+	}
+}
+
+public class Exercise {
+	public static void main(String[] args) {
+		Tv t = new Tv();
+	}
+}
